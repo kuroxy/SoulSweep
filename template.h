03ff3497d2d96212a1a3996def6b4be8847e11a9
Okke Roelofsen <okkeroelofsen@gmail.com>
2024-02-10 23:39:35 +0100

    fixed some warnings

--- template.h
+++ template.h
@@ -80,7 +80,7 @@ class vec2 // adapted from https://github.com/dcow/RayTracer
 {
 public:
 	union { struct { float x, y; }; float cell[2]; };
-	vec2() {}
+	vec2() : x{ 0.f }, y{ 0.f } {}
 	vec2( float v ) : x( v ), y( v ) {}
 	vec2( float x, float y ) : x( x ), y( y ) {}
 	vec2 operator - () const { return vec2( -x, -y ); }
@@ -105,7 +105,7 @@ class vec3
 {
 public:
 	union { struct { float x, y, z, dummy; }; float cell[4]; };
-	vec3() {}
+	vec3() : x{ 0.f }, y{ 0.f }, z{ 0.f } {}
 	vec3( float v ) : x( v ), y( v ), z( v ) {}
 	vec3( float x, float y, float z ) : x( x ), y( y ), z( z ) {}
 	vec3 operator - () const { return vec3( -x, -y, -z ); }
@@ -134,7 +134,7 @@ class vec4
 {
 public:
 	union { struct { float x, y, z, w; }; struct { vec3 xyz; float w2; }; float cell[4]; };
-	vec4() {}
+	vec4() : x{ 0.f }, y{ 0.f }, z{ 0.f }, w{ 0.f } {}
 	vec4( float v ) : x( v ), y( v ), z( v ), w( v ) {}
 	vec4( float x, float y, float z, float w ) : x( x ), y( y ), z( z ), w( w ) {}
 	vec4( vec3 a, float b ) : x( a.x ), y( a.y ), z( a.z ), w( b ) {}
@@ -168,7 +168,7 @@ class uint4
 {
 public:
 	union { struct { uint x, y, z, w; }; uint cell[4]; };
-	uint4() {}
+	uint4(): x{ 0 }, y{ 0 }, z{ 0 }, w{ 0 } {}
 	uint4( int v ) : x( v ), y( v ), z( v ), w( v ) {}
 	uint4( int x, int y, int z, int w ) : x( x ), y( y ), z( z ), w( w ) {}
 	uint4 operator + ( const uint4& addOperand ) const { return uint4( x + addOperand.x, y + addOperand.y, z + addOperand.z, w + addOperand.w ); }
@@ -186,7 +186,7 @@ class int4
 {
 public:
 	union { struct { int x, y, z, w; }; int cell[4]; };
-	int4() {}
+	int4() : x{ 0 }, y{ 0 }, z{ 0 }, w{ 0 } {}
 	int4( int v ) : x( v ), y( v ), z( v ), w( v ) {}
 	int4( int x, int y, int z, int w ) : x( x ), y( y ), z( z ), w( w ) {}
 	int4 operator - () const { return int4( -x, -y, -z, -w ); }
